080cf8db32cf7da66900f1d516e7a36aa990b157
Rohit Dhingra <er.rohit.dhingra@gmail.com>
2020-12-05 01:48:51 +0530

    Neo4j basic settings

---
+++ spring-graph-neo4j/src/test/java/com/example/demo/SpringGraphNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphNeo4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
